d9715f57d88b1f45dbfb0fb5c4cdd1910f16cae5
Kevin A <kevinatiyeh@gmail.com>
2025-07-18 20:23:51 -0400

    test

--- packages/nextjs/src/index.ts
+++ packages/nextjs/src/index.ts
@@ -2,7 +2,6 @@
 export { createSpoolHandler } from './handlers/spool-handler';
 export { getSpoolContent, getSpoolCollections, getSpoolSitemap, getSpoolRobots, generateSpoolMetadata } from './utils/content';
 export { SpoolSEO } from './components/spool-seo';
-export { createSpoolRoute } from './cli';
 
 // Types
 export type { SpoolConfig, SpoolContent, SpoolCollection } from './types'; 
